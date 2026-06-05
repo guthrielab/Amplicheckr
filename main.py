62df1ca5629ca09ed6f9dd018f0642e43534165c
dhuan6 <dhuan6@uwo.ca>
2026-06-05 16:25:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
     parser.add_argument("-a", "--align", action="store_true",
                         help="Only perform alignment and skip grading.")
     
-    #args = parser.parse_args()
+    args = parser.parse_args()
    
 
     primerin = args.primers
